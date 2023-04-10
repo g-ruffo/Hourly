c16d644c3fe274ef69d3cf4cad08281659679fde
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-04-10 16:26:20 -0700

    feat: Create constants file and add colour resource

---
+++ Hourly/Utils/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  Hourly
+//
+//  Created by Grayson Ruffo on 2023-04-10.
+//
+
+import Foundation
+
+
+struct K {
+    
+    struct Identifiers {
+        static let summaryNav = "SummaryNavigation"
+        static let calendarNav = "CalendarNavigation"
+        static let clientsNav = "ClientsNavigation"
+        static let workdayNav = "WorkdayNavigation"
+        static let settingsNav = "SettingsNavigation"
+
+    }
+}
