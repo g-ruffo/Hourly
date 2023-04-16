36d7b5ed6686191260e1c0383c8256e0ff9d57a0
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-04-16 13:09:52 -0700

    feat: Create delegate to format pay rate currency

--- Hourly/Model/AddEditWorkdayManager.swift
+++ Hourly/Model/AddEditWorkdayManager.swift
@@ -5,9 +5,45 @@
 //  Created by Grayson Ruffo on 2023-04-15.
 //
 
-import Foundation
+import UIKit
+
+protocol AddEditWorkdayManagerDelegate: UITextFieldDelegate {
+
+    func didUpdateCurrencyText(_ addEditWorkdayManager: AddEditWorkdayManager, newCurrencyValue: String?)
+}
 
 struct AddEditWorkdayManager {
+    
+    private var payRateAmount = 0
+    
+    var delegate: AddEditWorkdayManagerDelegate?
+    
+    
+    func updateAmount() -> String? {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = NumberFormatter.Style.currency
+        let amount = Double(payRateAmount / 100) + Double(payRateAmount % 100) / 100
+        return formatter.string(from: NSNumber(value: amount))
+
+    }
+    
+    mutating func validateCurrencyInput(string: String) -> Bool {
+        if payRateAmount >= 1000000 && string != "" {
+            return false
+        }
+        if let digit = Int(string) {
+            payRateAmount = payRateAmount * 10 + digit
+            delegate?.didUpdateCurrencyText(self, newCurrencyValue: updateAmount())
+
+        }
+        if string == ""
+        {
+            payRateAmount = payRateAmount / 10
+            delegate?.didUpdateCurrencyText(self, newCurrencyValue: updateAmount())
+        }
+        
+        return false
+    }
     
     func calculateEarnings(startTime: Date?, endTime: Date?, lunchTime: Int?, payRate: Double?) -> Double {
         if let start = startTime, let end = endTime, let rate = payRate {
