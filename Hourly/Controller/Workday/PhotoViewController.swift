21806194c42f7ba5a68259d6749a600aa3587a03
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-04-21 12:22:06 -0700

    feat: Add IBActions for edit buttons and delegate to inform parent of changes

--- Hourly/Controller/Workday/PhotoViewController.swift
+++ Hourly/Controller/Workday/PhotoViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+protocol PhotoCollectionDelegate {
+    func photoHasBeenDeleted(_ photoViewController: PhotoViewController, index: Int)
+}
+
 class PhotoViewController: UIViewController {
     
     var photos: Array<PhotoItem> = []
@@ -14,9 +18,13 @@ class PhotoViewController: UIViewController {
     var allowEditing = false
     private var hasSetToIndexPath = false
     
+    var delegate: PhotoCollectionDelegate?
+    
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var deleteButton: UIButton!
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    private let databaseContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +40,41 @@ class PhotoViewController: UIViewController {
         if !hasSetToIndexPath {
             collectionView.scrollToItem(at: IndexPath(row: startingRow!, section: 0), at: .top, animated: true)
             hasSetToIndexPath = true
+        }
+    }
+    
+    @IBAction func deleteButtonPressed(_ sender: UIButton) {
+        guard let index = collectionView.indexPathsForVisibleItems.first?.row else {
+            print("Error deleting photo")
+            return }
+        deletePhotoFromDatabase(index: index)
+        
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+        if savePhotoToDatabase() {
+            dismiss(animated: true)
+        }
+    }
+    
+    func deletePhotoFromDatabase(index: Int) {
+        databaseContext.delete(photos[index])
+        photos.remove(at: index)
+        collectionView.reloadData()
+        delegate?.photoHasBeenDeleted(self, index: index)
+    }
+    
+    func savePhotoToDatabase() -> Bool {
+        if databaseContext.hasChanges {
+            do {
+                try databaseContext.save()
+                return true
+            } catch {
+                print("Error saving workday to database = \(error)")
+                return false
+            }
+        } else {
+            return true
         }
     }
 }
@@ -50,19 +93,25 @@ extension PhotoViewController: UICollectionViewDataSource {
             cell.imageView.image = photo
         }
         cell.textView.text = photos[indexPath.row].imageDescription
+        cell.textView.delegate = self
+        cell.textView.tag = indexPath.row
         cell.setEditingState(allowEditing)
         return cell
     }
-}
-
-//MARK: - UICollectionViewDelegate
-extension PhotoViewController: UICollectionViewDelegate {
-    
 }
 
 //MARK: - UICollectionViewFlowLayout
 extension PhotoViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: collectionView.bounds.width, height: collectionView.bounds.height)
+    }
+}
+
+//MARK: - UITextViewDelegate
+extension PhotoViewController: UITextViewDelegate {
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if let text = textView.text {
+            photos[textView.tag].imageDescription = text
+        }
     }
 }
