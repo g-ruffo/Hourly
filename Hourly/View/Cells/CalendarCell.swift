f178101d2c2cc66d265d2e19fe51b8d109ebe98e
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-04-21 17:27:54 -0700

    feat: Insert image views into calendar cell stack view

--- Hourly/View/Cells/CalendarCell.swift
+++ Hourly/View/Cells/CalendarCell.swift
@@ -15,6 +15,34 @@ class CalendarCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        dayOfMonthLabel.layer.cornerRadius = dayOfMonthLabel.frame.height / 2
+        dayOfMonthLabel.clipsToBounds = true
     }
+    
+    func configure(title: String, with days: [WorkdayItem]) {
+        dayOfMonthLabel.text = title
+        if days.count > 0 {
+            for day in days {
+                let image = UIImageView(image: UIImage(systemName: "square.fill"))
+                image.tintColor = .clear
 
+                var colour = UIColor()
+                
+                if let tagColour = day.client?.tagColor { colour = UIColor(tagColour) }
+                else { colour = .gray }
+                
+                if day.isFinalized {
+                    image.backgroundColor = colour.withAlphaComponent(0.8)
+                } else {
+                    image.backgroundColor = colour.withAlphaComponent(0.2)
+                }
+                image.layer.cornerRadius = (image.frame.height / 2) / CGFloat(days.count)
+                stackView.addArrangedSubview(image)
+                
+            }
+        } else {
+            stackView.subviews.forEach { $0.removeFromSuperview()}
+        }
+    }
 }
+
