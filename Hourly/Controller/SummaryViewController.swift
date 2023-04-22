151593084540eedf3c9dc196b4281543d3126fb2
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-04-22 16:12:01 -0700

    feat: Connect IBOutlets to view controller and add table view extensions

--- Hourly/Controller/SummaryViewController.swift
+++ Hourly/Controller/SummaryViewController.swift
@@ -9,13 +9,37 @@ import UIKit
 
 class SummaryViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var earningsLabel: UILabel!
+    @IBOutlet weak var filterResultsButton: UIButton!
+    
     let databaseContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var workdays: Array<WorkdayItem> = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
     }
+    
+}
+
+extension SummaryViewController: UITableViewDelegate {
+    
+}
+
+extension SummaryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
     
 }
 
