e37ce5e27279533441d435e4b7c6d97622e38cd4
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-04-22 18:48:27 -0700

    feat: Create methods to get date ranges

--- Hourly/Model/SummaryManager.swift
+++ Hourly/Model/SummaryManager.swift
@@ -11,6 +11,16 @@ struct SummaryManager {
     
     private let calendar = Calendar.current
     
+    func firstOfWeek(date: Date) -> Date {
+        let components = calendar.dateComponents([.calendar, .yearForWeekOfYear, .weekOfYear], from: date)
+        return calendar.date(from: components)!
+    }
+    
+    func lastOfWeek(date: Date) -> Date {
+        let lastDay = calendar.date(byAdding: DateComponents(day: 6), to: firstOfWeek(date: date))
+        return lastDay!
+    }
+    
     func firstOfMonth(date: Date) -> Date {
         let components = calendar.dateComponents([.year, .month], from: date)
         return calendar.date(from: components)!
@@ -22,12 +32,12 @@ struct SummaryManager {
     }
     
     func firstOfYear(date: Date) -> Date {
-        let components = calendar.dateComponents([.year, .day], from: date)
+        let components = calendar.dateComponents([.year, .day], from: firstOfMonth(date: date))
         return calendar.date(from: components)!
     }
     
     func lastOfYear(date: Date) -> Date {
-        let lastDay = calendar.date(byAdding: DateComponents(year: 1, day: -1), to: firstOfMonth(date: date))
+        let lastDay = calendar.date(byAdding: DateComponents(month: 12, day: -1), to: firstOfYear(date: date))
         return lastDay!
     }
 
