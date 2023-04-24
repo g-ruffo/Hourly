6b1ef960b7a6c6d61abbcf93a41818881ec44cf7
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-04-24 13:20:53 -0700

    feat: Connect text field and date pickers to IBOutlets

--- Hourly/Controller/ExportViewController.swift
+++ Hourly/Controller/ExportViewController.swift
@@ -6,24 +6,52 @@
 //
 
 import UIKit
+import CoreData
 
 class ExportViewController: UIViewController {
+    
+    
+    @IBOutlet weak var clientTextField: ClientSearchTextField!
+    @IBOutlet weak var startDatePicker: UIDatePicker!
+    @IBOutlet weak var endDatePicker: UIDatePicker!
+    
+    private var selectedClient: ClientItem?
+    
+    private var selectedClientID: NSManagedObjectID? {
+        didSet {
+            if let id = selectedClientID {
+                do {
+                    selectedClient = try databaseContext.existingObject(with: id) as? ClientItem
+                } catch {
+                    fatalError(error.localizedDescription)
+                }
+            } else {
+                if selectedClient != nil {
+                    selectedClient = nil
+                }
+            }
+        }
+    }
+    
+    private let databaseContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        clientTextField.searchDelegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func exportButtonPressed(_ sender: UIButton) {
+        
     }
-    */
+    
+    
+}
 
+
+//MARK: - ClientSearchDelegate
+extension ExportViewController: ClientSearchDelegate {
+    func selectedExistingClient(_ clientSearchTextField: ClientSearchTextField, clientID: NSManagedObjectID?) {
+        self.selectedClientID = clientID
+    }
 }
