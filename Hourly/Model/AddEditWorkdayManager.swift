45fc55a65d29eb6911597c5707cf5e61e5c2bd6d
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-04-24 19:36:41 -0700

    refactor: Move calculate hours method into manager

--- Hourly/Model/AddEditWorkdayManager.swift
+++ Hourly/Model/AddEditWorkdayManager.swift
@@ -92,4 +92,14 @@ struct AddEditWorkdayManager {
         }
         return resultTime
     }
+    
+    func calculateHours(startTime: Date?, endTime: Date?, lunchTime: Int?) -> Double {
+        if let start = startTime, let end = endTime {
+            let timeWorked = Int(end.timeIntervalSince1970 - start.timeIntervalSince1970)
+            let hours = (Double(timeWorked) - Double(lunchTime ?? 0)) / 3600
+            return abs(hours)
+        } else {
+            return 0
+        }
+    }
 }
--- Hourly/Utils/Helper.swift
+++ Hourly/Utils/Helper.swift
@@ -17,13 +17,4 @@ class Helper {
             return "--:--"
         }
     }
-    static func calculateHours(startTime: Date?, endTime: Date?, lunchTime: Int?) -> Double {
-        if let start = startTime, let end = endTime {
-            let timeWorked = Int(end.timeIntervalSince1970 - start.timeIntervalSince1970)
-            let hours = (Double(timeWorked) - Double(lunchTime ?? 0)) / 3600
-            return abs(hours)
-        } else {
-            return 0
-        }
-    }
 }
