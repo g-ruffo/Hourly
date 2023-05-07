a96a838ed9dc3ce41b681fbffa53844e91f898e6
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-05-07 15:39:06 -0700

    test: Implement protocol in mock

--- HourlyTests/Controller/Model/Data/CoreDataServiceMock.swift
+++ HourlyTests/Controller/Model/Data/CoreDataServiceMock.swift
@@ -9,13 +9,63 @@ import Foundation
 @testable import Hourly
 import CoreData
 
-class CoreDataServiceMock: CoreDataServiceDelegate {
+final class CoreDataServiceMock: CoreDataServiceProtocol {
     weak var delegate: CoreDataServiceDelegate?
 
+    func createUpdateWorkday(clientName: String, date workDate: Date, start: Date?, end: Date?, lunch: Int32, mileage: Int32, rate: Double, location: String?, description: String?, timeWorked: Int32, earnings: Double, isDraft: Bool) -> Bool {
+        return true
+    }
+    
+    func addPhotoItemsToWorkday(_ workday: WorkdayItem) {
+        
+    }
+    
+    func createPhotoItems(from jpegImages: Array<Data?>) {
+        
+    }
+    
+    func deleteWorkday() -> Bool {
+        return true
+        
+    }
+    
+    func deletePhoto(at index: Int) {
+        
+    }
+    
+    func updatePhoto(at index: Int, text: String) {
+        
+    }
+    
+    func getClientFromURL(url: URL) {
+        
+    }
+    
+    func getWorkdayFromObjectId(_ id: NSManagedObjectID) {
+        
+    }
+    func getWorkdays(withRequest request : NSFetchRequest<WorkdayItem>) {
+        
+    }
+    
+    func getWorkdayClientID() -> URL? {
+        return nil
+    }
+    
+    func getClients(withRequest request : NSFetchRequest<ClientItem>) {
+        
+    }
+    
     func getClientFromID(_ id: NSManagedObjectID?) {
-        let client = ClientItem()
-        client.companyName = "Superman"
-        client.contactName = "Eric"
-        client.phoneNumber = "6049999999"
+        
+    }
+    
+    func deleteClient() -> Bool {
+        return true
+        
+    }
+    
+    func createUpdateClient(companyName: String, contactName: String?, phone: String?, email: String?, address: String?, rate: Double, tagColour: String?) -> Bool {
+        return true
     }
 }
